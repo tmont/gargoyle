e32f32d500710d504aa35c4e5a8c1e8352fd2c2d
tmont <tommy.mont@gmail.com>
2013-04-16 17:01:33 -0700

    moved tests around

--- tests/monitor-tests.js
+++ tests/monitor-tests.js
@@ -38,67 +38,49 @@ describe('Monitoring', function() {
 		});
 	}
 
-	it('should detect change to file in root directory', function(done) {
-		var file = path.join(root, 'foo.txt');
-		fs.writeFile(file, 'foo', function(err) {
-			should.not.exist(err);
-			vigilia.monitor(root, function(err, context) {
+	describe('on a directory', function() {
+		it('should detect change to file in root directory', function(done) {
+			var file = path.join(root, 'foo.txt');
+			fs.writeFile(file, 'foo', function(err) {
 				should.not.exist(err);
-				watcher = context;
-				watcher.monitor.on('update', function(filename) {
-					filename.should.equal(file);
-					done();
-				});
-				ensureNoEvents('create', 'delete', 'rename', done);
-
-				fs.appendFile(file, 'bar', function(err) {
+				vigilia.monitor(root, function(err, context) {
 					should.not.exist(err);
+					watcher = context;
+					watcher.monitor.on('update', function(filename) {
+						filename.should.equal(file);
+						done();
+					});
+					ensureNoEvents('create', 'delete', 'rename', done);
+
+					fs.appendFile(file, 'bar', function(err) {
+						should.not.exist(err);
+					});
 				});
 			});
 		});
-	});
 
-	it('should detect change to file in subdirectory', function(done) {
-		var file = path.join(root, 'foo/bar/baz/foo.txt');
-		fs.createFile(file, function(err) {
-			should.not.exist(err);
-			vigilia.monitor(root, function(err, context) {
-				should.not.exist(err);
-				watcher = context;
-				watcher.monitor.on('update', function(filename) {
-					filename.should.equal(file);
-					done();
-				});
-				ensureNoEvents('create', 'delete', 'rename', done);
-
-				fs.appendFile(file, 'bar', function(err) {
-					should.not.exist(err);
-				});
-			});
-		});
-	});
-
-	it('should detect created file in root directory', function(done) {
-		var file = path.join(root, 'foo.txt');
-		vigilia.monitor(root, function(err, context) {
-			should.not.exist(err);
-			watcher = context;
-			watcher.monitor.on('create', function(filename) {
-				filename.should.equal(file);
-				done();
-			});
-			ensureNoEvents('update', 'delete', 'rename', done);
-
+		it('should detect change to file in subdirectory', function(done) {
+			var file = path.join(root, 'foo/bar/baz/foo.txt');
 			fs.createFile(file, function(err) {
 				should.not.exist(err);
+				vigilia.monitor(root, function(err, context) {
+					should.not.exist(err);
+					watcher = context;
+					watcher.monitor.on('update', function(filename) {
+						filename.should.equal(file);
+						done();
+					});
+					ensureNoEvents('create', 'delete', 'rename', done);
+
+					fs.appendFile(file, 'bar', function(err) {
+						should.not.exist(err);
+					});
+				});
 			});
 		});
-	});
 
-	it('should detect created file in subdirectory', function(done) {
-		var file = path.join(root, 'foo/bar/baz/foo.txt');
-		fs.mkdirs(path.join(root, 'foo/bar/baz'), function(err) {
-			should.not.exist(err);
+		it('should detect created file in root directory', function(done) {
+			var file = path.join(root, 'foo.txt');
 			vigilia.monitor(root, function(err, context) {
 				should.not.exist(err);
 				watcher = context;
@@ -113,117 +95,137 @@ describe('Monitoring', function() {
 				});
 			});
 		});
-	});
 
-	it('should detect deleted file in root directory', function(done) {
-		var file = path.join(root, 'foo.txt');
-		fs.createFile(file, function(err) {
-			should.not.exist(err);
-			vigilia.monitor(root, function(err, context) {
+		it('should detect created file in subdirectory', function(done) {
+			var file = path.join(root, 'foo/bar/baz/foo.txt');
+			fs.mkdirs(path.join(root, 'foo/bar/baz'), function(err) {
 				should.not.exist(err);
-				watcher = context;
-				watcher.monitor.on('delete', function(filename) {
-					filename.should.equal(file);
-					done();
-				});
-				ensureNoEvents('create', 'update', 'rename', done);
-
-				fs.unlink(file, function(err) {
+				vigilia.monitor(root, function(err, context) {
 					should.not.exist(err);
-				});
-			});
-		});
-	});
-
-	it('should detect deleted file in subdirectory', function(done) {
-		var file = path.join(root, 'foo/bar/baz/foo.txt');
-		fs.createFile(file, function(err) {
-			should.not.exist(err);
-			vigilia.monitor(root, function(err, context) {
-				should.not.exist(err);
-				watcher = context;
-				watcher.monitor.on('delete', function(filename) {
-					filename.should.equal(file);
-					done();
-				});
-				ensureNoEvents('create', 'update', 'rename', done);
-
-				fs.unlink(file, function(err) {
-					should.not.exist(err);
-				});
-			});
-		});
-	});
-
-	it('should detect renamed file in root directory', function(done) {
-		var file = path.join(root, 'foo.txt');
-		fs.createFile(file, function(err) {
-			should.not.exist(err);
-			vigilia.monitor(root, function(err, context) {
-				should.not.exist(err);
-				watcher = context;
-				watcher.monitor.on('rename', function(filename) {
-					filename.should.equal(file);
-					done();
-				});
-				ensureNoEvents('delete', 'update', done);
-
-				var newFile = path.join(path.dirname(file), 'bar.txt');
-				fs.rename(file, newFile, function(err) {
-					should.not.exist(err);
-				});
-			});
-		});
-	});
-
-	it('should detect renamed file in subdirectory', function(done) {
-		var file = path.join(root, 'foo/bar/baz/foo.txt');
-		fs.createFile(file, function(err) {
-			should.not.exist(err);
-			vigilia.monitor(root, function(err, context) {
-				should.not.exist(err);
-				watcher = context;
-				watcher.monitor.on('rename', function(filename) {
-					filename.should.equal(file);
-					done();
-				});
-				ensureNoEvents('delete', 'update', done);
-
-				var newFile = path.join(path.dirname(file), 'bar.txt');
-				fs.rename(file, newFile, function(err) {
-					should.not.exist(err);
-				});
-			});
-		});
-	});
-
-	it('should emit "rename" and "create" for renamed files', function(done) {
-		var file = path.join(root, 'foo/bar/baz/foo.txt');
-		var newFile = path.join(path.dirname(file), 'bar.txt');
-		fs.createFile(file, function(err) {
-			should.not.exist(err);
-			vigilia.monitor(root, function(err, context) {
-				should.not.exist(err);
-				watcher = context;
-				var renamed = false, created = false;
-				watcher.monitor.on('rename', function(filename) {
-					filename.should.equal(file);
-					renamed = true;
-					if (created) {
+					watcher = context;
+					watcher.monitor.on('create', function(filename) {
+						filename.should.equal(file);
 						done();
-					}
-				});
-				watcher.monitor.on('create', function(filename) {
-					filename.should.equal(newFile);
-					created = true;
-					if (renamed) {
-						done();
-					}
-				});
-				ensureNoEvents('delete', 'update', done);
+					});
+					ensureNoEvents('update', 'delete', 'rename', done);
 
-				fs.rename(file, newFile, function(err) {
+					fs.createFile(file, function(err) {
+						should.not.exist(err);
+					});
+				});
+			});
+		});
+
+		it('should detect deleted file in root directory', function(done) {
+			var file = path.join(root, 'foo.txt');
+			fs.createFile(file, function(err) {
+				should.not.exist(err);
+				vigilia.monitor(root, function(err, context) {
 					should.not.exist(err);
+					watcher = context;
+					watcher.monitor.on('delete', function(filename) {
+						filename.should.equal(file);
+						done();
+					});
+					ensureNoEvents('create', 'update', 'rename', done);
+
+					fs.unlink(file, function(err) {
+						should.not.exist(err);
+					});
+				});
+			});
+		});
+
+		it('should detect deleted file in subdirectory', function(done) {
+			var file = path.join(root, 'foo/bar/baz/foo.txt');
+			fs.createFile(file, function(err) {
+				should.not.exist(err);
+				vigilia.monitor(root, function(err, context) {
+					should.not.exist(err);
+					watcher = context;
+					watcher.monitor.on('delete', function(filename) {
+						filename.should.equal(file);
+						done();
+					});
+					ensureNoEvents('create', 'update', 'rename', done);
+
+					fs.unlink(file, function(err) {
+						should.not.exist(err);
+					});
+				});
+			});
+		});
+
+		it('should detect renamed file in root directory', function(done) {
+			var file = path.join(root, 'foo.txt');
+			fs.createFile(file, function(err) {
+				should.not.exist(err);
+				vigilia.monitor(root, function(err, context) {
+					should.not.exist(err);
+					watcher = context;
+					watcher.monitor.on('rename', function(filename) {
+						filename.should.equal(file);
+						done();
+					});
+					ensureNoEvents('delete', 'update', done);
+
+					var newFile = path.join(path.dirname(file), 'bar.txt');
+					fs.rename(file, newFile, function(err) {
+						should.not.exist(err);
+					});
+				});
+			});
+		});
+
+		it('should detect renamed file in subdirectory', function(done) {
+			var file = path.join(root, 'foo/bar/baz/foo.txt');
+			fs.createFile(file, function(err) {
+				should.not.exist(err);
+				vigilia.monitor(root, function(err, context) {
+					should.not.exist(err);
+					watcher = context;
+					watcher.monitor.on('rename', function(filename) {
+						filename.should.equal(file);
+						done();
+					});
+					ensureNoEvents('delete', 'update', done);
+
+					var newFile = path.join(path.dirname(file), 'bar.txt');
+					fs.rename(file, newFile, function(err) {
+						should.not.exist(err);
+					});
+				});
+			});
+		});
+
+		it('should emit "rename" and "create" for renamed files', function(done) {
+			var file = path.join(root, 'foo/bar/baz/foo.txt');
+			var newFile = path.join(path.dirname(file), 'bar.txt');
+			fs.createFile(file, function(err) {
+				should.not.exist(err);
+				vigilia.monitor(root, function(err, context) {
+					should.not.exist(err);
+					watcher = context;
+					var renamed = false, created = false;
+					watcher.monitor.on('rename', function(filename) {
+						filename.should.equal(file);
+						renamed = true;
+						if (created) {
+							done();
+						}
+					});
+					watcher.monitor.on('create', function(filename) {
+						filename.should.equal(newFile);
+						created = true;
+						if (renamed) {
+							done();
+						}
+					});
+					ensureNoEvents('delete', 'update', done);
+
+					fs.rename(file, newFile, function(err) {
+						should.not.exist(err);
+					});
 				});
 			});
 		});
